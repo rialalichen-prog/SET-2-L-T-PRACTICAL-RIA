494eba8c2901d96e8aa9c2c9778b21525a80323a
rialalichen-prog <rialalichen@gmail.com>
2026-07-24 11:29:34 +0530

    Create TASK-6.js

---
+++ SET-2/TASK-6.js
@@ -0,0 +1,16 @@
+let patients = [
+    { name: "Rahul", age: 35 },
+    { name: "Anita", age: 42 },
+    { name: "Vikram", age: 28 },
+    { name: "Priya", age: 60 }
+];
+
+for (let patient of patients) {
+    console.log("Name:", patient.name, "Age:", patient.age);
+}
+
+// Sample Output:
+// Name: Rahul Age: 35
+// Name: Anita Age: 42
+// Name: Vikram Age: 28
+// Name: Priya Age: 60
