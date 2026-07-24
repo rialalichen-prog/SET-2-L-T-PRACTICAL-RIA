c425f5ab5be3a701a1e31e59b6ba10b3040c393b
rialalichen-prog <rialalichen@gmail.com>
2026-07-24 11:27:29 +0530

    Create TASK-4.js

---
+++ SET-2/TASK-4.js
@@ -0,0 +1,9 @@
+let patient1Age = 45;
+let patient2Age = 30;
+
+let ageDifference = patient1Age - patient2Age;
+
+console.log("Age Difference:", ageDifference);
+
+// Sample Output:
+// Age Difference: 15
