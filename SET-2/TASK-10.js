a4c66a2b5567a37a0fba4e46e22b4c1bfefd52a3
rialalichen-prog <rialalichen@gmail.com>
2026-07-24 11:31:18 +0530

    Create TASK-10.js

---
+++ SET-2/TASK-10.js
@@ -0,0 +1,20 @@
+let patient = {
+    id: 101,
+    name: "Rahul",
+    age: 35,
+    disease: "Fever",
+    admitted: true
+};
+
+console.log("ID:", patient.id);
+console.log("Name:", patient.name);
+console.log("Age:", patient.age);
+console.log("Disease:", patient.disease);
+console.log("Admitted:", patient.admitted);
+
+// Sample Output:
+// ID: 101
+// Name: Rahul
+// Age: 35
+// Disease: Fever
+// Admitted: true
